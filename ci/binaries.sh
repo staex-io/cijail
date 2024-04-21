85de0afc887432f4a615ae294ed86fd37564f729
Ivan Gankevich <github@capybaramail.xyz>
2024-04-21 19:04:08 +0200

    CI

--- ci/binaries.sh
+++ ci/binaries.sh
@@ -21,15 +21,14 @@ set -ex
 rootdir="$PWD"
 workdir="$(mktemp -d)"
 rust_flags="-Ccodegen-units=1 -Cstrip=symbols -Copt-level=3 -Cincremental=false -Clto=yes -Cembed-bitcode=yes"
-target=x86_64-unknown-linux-musl
+target=x86_64-unknown-linux-gnu
 export LIBSECCOMP_LINK_TYPE=static
 export LIBSECCOMP_LIB_PATH=/usr/lib/x86_64-linux-gnu
-apt-get update -qq
-apt-get install clang -y
+glibc_version="$(getconf GNU_LIBC_VERSION | sed 's/ /-/g')"
 env RUSTFLAGS="$rust_flags" \
     cargo build \
     --quiet \
     --release \
     --target "$target" \
     --no-default-features
-mv target/"$target"/release/cijail cijail
+mv target/"$target"/release/cijail cijail-"$glibc_version"
