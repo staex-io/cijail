b5a6bb61b94e429a89a12b4e666c194698ff4e29
Ivan Gankevich <github@capybaramail.xyz>
2024-04-27 21:17:15 +0200

    CI

--- ci/test.sh
+++ ci/test.sh
@@ -32,6 +32,7 @@ test_all() {
     cargo test --quiet --no-run
     ./target/debug/cijail --version
     ldd ./target/debug/cijail
+    ./target/debug/cijail sh -c 'head -n1 /etc/passwd'
     # run all tests
     cargo test --no-fail-fast -- --nocapture
 }
