14143b87ffbdc883ab178db389ab0c1c1be90cc3
Ivan Gankevich <github@capybaramail.xyz>
2024-04-28 09:41:08 +0200

    CI

--- ci/binaries.sh
+++ ci/binaries.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 set -ex
+if test "$GITHUB_REF_TYPE" != "tag"; then
+    exit 0
+fi
 rust_flags="-Ccodegen-units=1 -Cstrip=symbols -Copt-level=3 -Cincremental=false -Clto=yes -Cembed-bitcode=yes"
 target=x86_64-unknown-linux-gnu
 export LIBSECCOMP_LINK_TYPE=static
